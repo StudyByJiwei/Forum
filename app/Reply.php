b86d2f3bd92f5e967b68a0f0a36da92fec882b54
yiqiao <208274887@qq.com>
2018-09-08 23:22:34 +0800

    18-A User Can Favorite Replies

--- app/Reply.php
+++ app/Reply.php
@@ -8,8 +8,27 @@ class Reply extends Model
 {
     protected $guarded = [];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+        if (! $this->favorites()->where($attributes)->exists()) {
+            $this->favorites()->create($attributes);
+        }
     }
 }
