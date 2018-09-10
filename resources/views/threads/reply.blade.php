fd3ad2b2699f8f1c84eb31eedf4ac37d7adb34ff
yiqiao <208274887@qq.com>
2018-09-10 22:26:30 +0800

    20-From 56 Queries Down to 2

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -12,7 +12,7 @@
                 <form action="/replies/{{ $reply->id }}/favorites" method="post">
                     {{ csrf_field() }}
                     <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favorites()->count() }} {{ str_plural('favorite', $reply->favorites()->count()) }}
+                        {{ $reply->favorites_count }} {{ str_plural('favorite', $reply->favorites_count) }}
                     </button>
                 </form>
             </div>
