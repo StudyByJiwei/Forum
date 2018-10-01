8efcfb0509fc598572b954fa5bb196c88d338848
yiqiao <208274887@qq.com>
2018-10-01 20:57:47 +0800

    39-A User Can Filter By Unanswered Threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -35,8 +35,7 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
-        $this->get($this->thread->path())
-            ->assertSee($reply->body);
+        $this->assertDatabaseHas('replies', ['body' => $reply->body]);
     }
 
     /** @test */
@@ -79,6 +78,15 @@ class ReadThreadsTest extends TestCase
     }
     
     /** @test */
+    function a_user_can_filter_threads_by_those_that_are_unaswered()
+    {
+        $thread = create('App\Thread');
+        create('App\Reply', ['thread_id' => $thread->id]);
+        $response = $this->getJson('threads?unanswered=1')->json();
+        $this->assertCount(1, $response);
+    }
+    
+    /** @test */
     function a_user_can_request_all_replies_for_a_given_thread()
     {
         $thread = create('App\Thread');
@@ -86,7 +94,7 @@ class ReadThreadsTest extends TestCase
 
         $response = $this->getJson($thread->path() . '/replies')->json();
 
-        $this->assertCount(1, $response['data']);
+        $this->assertCount(2, $response['data']);
         $this->assertEquals(2, $response['total']);
     }
 }
