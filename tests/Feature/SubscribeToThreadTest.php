d9f20d68763821e9c7bc4dcd7d9a8e21e86029c6
yiqiao <208274887@qq.com>
2018-10-03 22:10:19 +0800

    41-Thread Subscriptions Part 2

---
+++ tests/Feature/SubscribeToThreadTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class SubscribeToThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+        $thread = create('App\Thread');
+        $this->post($thread->path(), '/subscriptions');
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body'    => 'Some reply here'
+        ]);
+        $this->assertCount(1, auth()->user()->notifications);
+    }
+}
