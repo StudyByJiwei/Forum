78c73e55edfc570d4c9d5c81fd6862dbb1f80249
yiqiao <208274887@qq.com>
2018-10-09 21:03:40 +0800

    46-Refactoring for the Better or Worse

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Events\ThreadHasNewReply;
 use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
 
@@ -79,13 +80,17 @@ class Thread extends Model
     public function addReply($reply)
     {
         $reply = $this->replies()->create($reply);
-        $this->subscriptions
-            ->filter(function($sub) use ($reply) {
-                return $sub->user_id != $reply->user_id;
-            })->each->notify($reply);
-
+        $this->notifySubscribers($reply);
 
         return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
+        $this->subscriptions
+            ->where('user_id', '!=', $reply->user_id)
+            ->each
+            ->notify($reply);
     }
 
     /**
