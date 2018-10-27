3f55cb0cc4de14b886cb5f59901b292e573c272f
yiqiao <208274887@qq.com>
2018-10-27 22:49:24 +0800

    62-Basic View Tweaks

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,34 +4,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                @forelse($threads as $thread)
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <div class="level">
-                                <h4 class="flex">
-                                    <a href="{{ $thread->path() }}">
-                                        @if(auth()->user() && $thread->hasUpdatesFor(auth()->user()))
-                                            <strong>{{ $thread->title }}</strong>
-                                        @else
-                                            {{ $thread->title }}
-                                        @endif
-                                    </a>
+                @include('threads._list')
 
-                                </h4>
-
-                                <a href="{{ $thread->path() }}">
-                                    <strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="panel-body">
-                                <div class="body">{{ $thread->body }}</div>
-                        </div>
-                    </div>
-                @empty
-                    <p>There are no relevant result at this time.</p>
-                @endforelse
+                {{ $threads->render() }}
             </div>
         </div>
     </div>
