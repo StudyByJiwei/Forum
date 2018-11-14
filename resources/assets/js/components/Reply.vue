c13cb44f1509250394361cc2d9ecbf23157f549a
yiqiao <208274887@qq.com>
2018-11-14 20:50:10 +0800

    79-Mark the Best Reply Part 2

--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :id="'reply-'+id" class="panel panel-default">
+    <div :id="'reply-'+id" class="panel " :class="isBest ? 'panel-success' : 'panel-default'">
         <div class="panel-heading">
             <div class="level">
                 <h5 class="flex">
@@ -28,9 +28,12 @@
             <div v-else v-html="body"></div>
         </div>
 
-        <div class="panel-footer level" v-if="canUpdate">
-            <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
-            <button class="btn btn-danger btn-xs mr-1" @click="destroy">Delete</button>
+        <div class="panel-footer level">
+            <div v-if="canUpdate">
+                <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
+                <button class="btn btn-danger btn-xs mr-1" @click="destroy">Delete</button>
+            </div>
+            <button class="btn btn-default btn-xs ml-a" @click="markBestReply" v-show="! isBest">Best Reply?</button>
         </div>
     </div>
 
@@ -47,6 +50,7 @@
                 editing: false,
                 id: this.data.id,
                 body: this.data.body,
+                isBest:false
             };
         },
         computed: {
@@ -77,6 +81,10 @@
             destroy() {
                 axios.delete('/replies/' + this.data.id);
                 this.$emit('deleted', this.data.id);
+            },
+            markBestReply() {
+                this.isBest = true;
+
             }
         }
     }
