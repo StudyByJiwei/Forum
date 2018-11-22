486f2342dcf6656fe38bed44580b3e698bd6bf13
yiqiao <208274887@qq.com>
2018-11-22 19:21:33 +0800

    84-An Administrator May Lock Any Thread

--- app/Thread.php
+++ app/Thread.php
@@ -82,6 +82,11 @@ class Thread extends Model
         return $reply;
     }
 
+    public function lock()
+    {
+        $this->update(['locked' => true]);
+    }
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
