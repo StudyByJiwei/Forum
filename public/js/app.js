df7863e4a08d165602e6ed0b19dc6147791037ff
yiqiao <208274887@qq.com>
2018-11-24 23:26:42 +0800

    86-An Administrator May Lock Any Thread Part 3

--- public/js/app.js
+++ public/js/app.js
@@ -58685,6 +58685,9 @@ module.exports = {
         var prop = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'user';
 
         return model[prop] === user.id;
+    },
+    isAdmin: function isAdmin() {
+        return ['YiQiao'].includes(user.name);
     }
 };
 
@@ -60492,11 +60495,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-    props: ['initialRepliesCount'],
+    props: ['dataRepliesCount', 'dataLocked'],
     components: { Replies: __WEBPACK_IMPORTED_MODULE_0__components_Replies_vue___default.a, SubscribeButton: __WEBPACK_IMPORTED_MODULE_1__components_SubscribeButton_vue___default.a },
     data: function data() {
         return {
-            repliesCount: this.initialRepliesCount
+            repliesCount: this.dataRepliesCount,
+            locked: this.dataLocked
         };
     }
 });
@@ -60559,6 +60563,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__NewReply_vue__ = __webpack_require__(189);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__NewReply_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__NewReply_vue__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__mixins_collection__ = __webpack_require__(194);
+//
+//
 //
 //
 //
@@ -63207,7 +63213,13 @@ var render = function() {
         on: { changed: _vm.fetch }
       }),
       _vm._v(" "),
-      _c("new-reply", { on: { created: _vm.add } })
+      _vm.$parent.locked
+        ? _c("p", [
+            _vm._v(
+              "\n        This thread has been locked.No more replies are allowed.\n    "
+            )
+          ])
+        : _c("new-reply", { on: { created: _vm.add } })
     ],
     2
   )
--- resources/assets/js/authorizations.js
+++ resources/assets/js/authorizations.js
@@ -1,8 +1,10 @@
 let user = window.App.user;
 
 module.exports = {
-
     owns(model, prop = 'user') {
         return model[prop] === user.id;
+    },
+    isAdmin () {
+        return ['YiQiao'].includes(user.name);
     }
 };
