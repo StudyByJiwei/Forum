51628598242cb20c43339b29d453f0a6af8041b1
yiqiao <208274887@qq.com>
2018-12-03 23:57:22 +0800

    95-First Class Search Implementation

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -10,6 +10,22 @@
             </div>
 
             <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Search
+                    </div>
+                    <div class="panel-body">
+                        <form action="/threads/search" method="get">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search for somethin..." name="q">
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-default">Search</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
                 @if(count($trending))
                     <div class="panel panel-default">
                         <div class="panel-heading">
